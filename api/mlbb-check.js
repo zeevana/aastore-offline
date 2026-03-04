832a5af26443695e004db9bd9dc68e70b25f5631
zeevana <sromdoni0@gmail.com>
2026-03-04 18:42:46 +0900

    api

--- api/mlbb-check.js
+++ api/mlbb-check.js
@@ -2,19 +2,24 @@ export default async function handler(req, res) {
 
   try {
 
-    const { userId, serverId } = req.body;
+    let { userId, serverId } = req.body;
+
+    // hapus karakter selain angka
+    serverId = serverId.replace(/[^0-9]/g, "");
 
     const url = `https://id-game-checker.p.rapidapi.com/mobile-legends/${userId}/${serverId}`;
 
     const response = await fetch(url, {
       method: "GET",
       headers: {
-        "x-rapidapi-key": "40a8e41047msh7798b550140be7ep1c654ajsndc857efa4ec7",
+        "x-rapidapi-key": process.env.RAPID_API_KEY,
         "x-rapidapi-host": "id-game-checker.p.rapidapi.com"
       }
     });
 
     const data = await response.json();
+
+    console.log("RapidAPI result:", data);
 
     if (!data || !data.username) {
 
