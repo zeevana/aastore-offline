cce3e3a81a93582503e32ddb576cccfb1010c077
zeevana <sromdoni0@gmail.com>
2026-03-08 17:55:02 +0900

    api

--- api/get-leaderboard.js
+++ api/get-leaderboard.js
@@ -1,31 +1,46 @@
-import clientPromise from "../src/lib/mongodb.js";
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URI;
+
+let client;
+let clientPromise;
+
+if (!uri) {
+  throw new Error("MONGO_URI belum diisi di ENV");
+}
+
+if (!global._mongoClientPromise) {
+
+  client = new MongoClient(uri);
+
+  global._mongoClientPromise = client.connect();
+
+}
+
+clientPromise = global._mongoClientPromise;
+
 
 export default async function handler(req, res) {
 
   try {
 
     const client = await clientPromise;
+
     const db = client.db("aastore");
 
     const players = await db
       .collection("mlbb_event")
       .find({})
-      .sort({ createdAt: -1 })
+      .sort({ createdAt: 1 }) // URUTKAN DARI PALING LAMA
       .toArray();
 
-    const result = players.map(p => ({
-      id: p._id,
-      nickname: p.nickname,
-      userId: p.userId
-    }));
-
-    res.status(200).json(result);
+    return res.status(200).json(players);
 
   } catch (error) {
 
-    console.log(error);
+    console.log("GET LEADERBOARD ERROR:", error);
 
-    res.status(500).json({
+    return res.status(500).json({
       error: "Database error"
     });
 
